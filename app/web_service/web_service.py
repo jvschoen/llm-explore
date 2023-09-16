2611378f8c4dd5edb4e4711efe91b548ed53540d
jvschoen <jvshoen@live.com>
2023-09-15 23:45:31 -0700

    pulled port from env var, adding different text parsing utils

--- app/web_service/web_service.py
+++ app/web_service/web_service.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+import os
+from io import BytesIO
+
+from docx import Document
+from pypdf import PdfReader
 
 app = Flask(__name__)
 
-EMBEDDING_SERVICE_URL = "http://embedder:5001/embed" # Embedding service on port 5001
-SEARCH_SERVICE_URL = 'http://searcher:5002/search'
-INGEST_DOC_SERVICE_URL = "http://ingestion_service:5004/ingest_document"
+EMBEDDING_SERVICE_URL = f'http://embedder:{os.environ["EMBEDDING_SERVICE_PORT"]}/embed' # Embedding service on port 5001
+SEARCH_SERVICE_URL = f'http://searcher:{os.environ["SEARCH_SERVICE_PORT"]}/search'
+INGEST_DOC_SERVICE_URL = f'http://ingestor:{os.environ["INGESTION_SERVICE_PORT"]}/ingest_document'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,8 +28,36 @@ def index():
             return render_template('results.html', document_ids=docs)
     return render_template('index.html')
 
+
+def extract_text_from_file(file):
+
+    filename = file.filename
+    print(type(file))
+
+    # Get file extension
+    file_extension = os.path.splitext(filename)[1].lower()
+    print(f'file extension: {file_extension}')
+
+    # For Word documents
+    if file_extension == '.docx':
+        doc = Document(BytesIO(file.read()))
+        return [p.text for p in doc.paragraphs if p.text.strip() != ""]
+
+    # For PDFs
+    elif file_extension == '.pdf':
+        reader = PdfReader(file)
+        return [reader.getPage(i).extractText() for i in range(reader.numPages)]
+
+    # For plain text files
+    # add more plain text formats if necessary
+    elif file_extension in ['.txt', '.md', '.csv', '.tsv']:
+        return file.read()
+
+    else:
+        raise ValueError(f"Unsupported file type: {file_extension}")
+
 def ingest_doc(file):
-    content = file.read().decode('utf-8')
+
     response = requests.post(INGEST_DOC_SERVICE_URL, json={'content': content})
     return jsonify(response.json())
 
@@ -39,4 +72,4 @@ def search_documents(embedding):
     return response.json().get('documents')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=os.environ['WEB_SERVICE_PORT'])
