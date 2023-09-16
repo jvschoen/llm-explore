7611b7029c148330c8f42906b22bf25d7305e510
jvschoen <jvshoen@live.com>
2023-09-15 23:44:59 -0700

    pulled port from env var, added functionality to read and write from faiss

--- app/search_service/search_service.py
+++ app/search_service/search_service.py
@@ -2,18 +2,28 @@
 from flask import Flask, request, jsonify
 import requests
 import atexit
+import os
 
 import numpy as np
 import faiss
 
-GET_DOC_IDS_URL = 'http://db_service:5003/get_document_ids'
-GET_DOCS_URL = 'http://db_service:5003/get_documents'
-ADD_MAPPING_URL = "http://db_service:5003/add_mapping"
+
+GET_DOC_IDS_URL = f'http://doc_db:{os.environ["DB_SERVICE_PORT"]}/get_document_ids'
+GET_DOCS_URL = f'http://doc_db:{os.environ["DB_SERVICE_PORT"]}/get_documents'
+ADD_MAPPING_URL = f'http://doc_db:{os.environ["DB_SERVICE_PORT"]}/add_mapping'
 
 app = Flask(__name__)
+INDEX_PATH = "/app/index_data/faiss_index.index"
 
-# Load the FAISS index (assuming it's persisted somewhere)
-index = faiss.read_index("faiss_index.index")
+def init_faiss_index():
+    if not os.path.exists(INDEX_PATH):
+        index = faiss.IndexFlatL2(384)
+        faiss.write_index(index, INDEX_PATH)
+
+
+def load_faiss_index():
+    return faiss.read_index(INDEX_PATH)
+
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -22,12 +32,14 @@ def search():
     to the user's input prompt
     """
     embedding = np.array(request.json['embedding']).reshape(1, -1)
+    index = load_faiss_index()
     _, I = index.search(embedding, 5) # Top 5 closest embeddings
+    indices = [int(index) for index in I[0]]
 
     # Pass over top 5 vector indices.
     # These indices map to source document ids
     response = requests.post(GET_DOC_IDS_URL,
-                             json={'indices': list(I[0])})
+                             json={'doc_ids': indices})
     matched_doc_ids = response.json().get('doc_ids')
     unique_doc_ids = list(set(matched_doc_ids))
 
@@ -43,6 +55,7 @@ def add_embedding():
     embedding = np.array(request.json['embedding']).reshape(1, -1)
     doc_id = request.json['doc_id']
 
+    index = load_faiss_index()
     faiss_index_position = index.ntotal
     index.add(embedding)
 
@@ -55,8 +68,11 @@ def add_embedding():
 
 # Ensure the FAISS index gets persisted after adding new data
 def save_faiss_index():
-    faiss.write_index(index, 'faiss_index.index')
+    index = load_faiss_index()
+    faiss.write_index(index, INDEX_PATH)
+
+init_faiss_index()
 
 if __name__ == '__main__':
     atexit.register(save_faiss_index)
-    app.run(host='0.0.0.0', port=5002)
+    app.run(host='0.0.0.0', port=os.environ['SEARCH_SERVICE_PORT'])
